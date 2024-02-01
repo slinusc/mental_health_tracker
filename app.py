757d96caf7e8550c4843b97f9b643b732b42bee8
Linus Stuhlmann <linus.stuhlmann@gmail.com>
2024-02-01 19:39:33 +0100

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import flask
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
@@ -17,12 +17,21 @@ def register():
         existing_user = users.find_one({'email': request.form['email']})
 
         if existing_user is None:
-            hashpass = generate_password_hash(request.form['password'])
-            users.insert_one(
-                {'username': request.form['username'], 'email': request.form['email'], 'password': hashpass})
-            return redirect(url_for('index'))
+            if request.form['password'] != request.form['confirm_password']:
+                session['password_match'] = False
+                return redirect(url_for('login_page', password_match='1'))
+            else:
+                hashpass = generate_password_hash(request.form['password'])
+                users.insert_one(
+                    {'username': request.form['username'], 'email': request.form['email'], 'password': hashpass})
+                return redirect(url_for('index'))
 
-        return 'That username already exists!'
+        if existing_user:
+            session['email_exists'] = True
+            return redirect(url_for('login_page', email_exists='1'))
+
+    return redirect(url_for('login_page'))
+
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -33,18 +42,21 @@ def login():
         login_user = users.find_one({'email': request.form['email']})
 
         if login_user is None:
-            return 'E-Mail not found'
+            flash('E-Mail oder Passwort inkorrekt', 'danger')
         elif not check_password_hash(login_user['password'], request.form['password']):
-            return 'Invalid password'
+            flash('E-Mail oder Passwort inkorrekt', 'danger')
         else:
             session['username'] = login_user['username']
             return redirect(url_for('index'))
+
+    return render_template('login_page.html')
+
 
 
 @app.route('/index')
 def index():
     if 'username' in session:
-        return flask.render_template('main_page.html', username=session['username'])
+        return render_template('main_page.html', username=session['username'])
     else:
         return redirect(url_for('login_page'))
 
@@ -57,12 +69,12 @@ def logout():
 
 @app.route('/')
 def login_page():
-    return flask.render_template('login_page.html')
+    return render_template('login_page.html')
 
 
 @app.route('/main')
 def main():
-    return flask.render_template('main_page.html')
+    return render_template('main_page.html')
 
 
 if __name__ == '__main__':
